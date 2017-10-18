4cf48f6162825c4dd821eb5c0498a27aeeeddabc
Leonardo Val <leonardo.val@creatartis.com>
2017-10-18 11:10:14 -0300

    Paths for Karma + Missing slash after `/base` for Karma configuration.

--- build/creatartis-grunt.js
+++ build/creatartis-grunt.js
@@ -197,7 +197,7 @@ var requireConfig = exports.requireConfig = function requireConfig(config) {
 			for (var p in config.paths) {
 				config.paths[p] = config.paths[p]
 					.replace(/\\/g, '/') // For paths on Windows.
-					.replace(/^\.\.\//, '/base');
+					.replace(/^\.\.\//, '/base/');
 			}
 			config.deps = Object.keys(window.__karma__.files) // Dynamically load all test files
 				.filter(function (file) { // Filter test modules.
